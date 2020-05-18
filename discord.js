42b876132fb9af459291589ffea4c3253e2a7ff4
Snakeshader1 <stoffelbach.theo@gmail.com>
2020-05-18 10:51:34 +0200

    exo 3
    exo 3 j'ai détruit les 2 avant

--- discord.js
+++ discord.js
@@ -1,22 +1,12 @@
-let i = 1;
+let index = vert,jaune,bleu;
+
 switch (i) { //i est la variable dont on va regarder la valeur
-    case 0:
-        console.log(0);
-        break; //mot-clé qui permet de sortir de ce cas pour
 
  // si pas de break : on passe au bloc suivant /!\
+    case 0:
+        break;
     case 1:
-        console.log(1);
+        console.log(`La couleur à l'index ${index} est : `);
         break;
-    case 2:
-        console.log(3);
-        break;
-    case 3:
-        console.log(4);
-        break;
-    case 4:
-        console.log(5);
-        break;
-    default:
-        console.log("(╯°□°)╯︵ ┻━┻");
+
 }
