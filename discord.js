f4236edf2e10f08d36d47be6e22b0ea825271fed
Snakeshader1 <stoffelbach.theo@gmail.com>
2020-05-18 10:46:09 +0200

    creation
    aa

---
+++ discord.js
@@ -0,0 +1,22 @@
+let i = 1;
+switch (i) { //i est la variable dont on va regarder la valeur
+    case 0:
+        console.log(0);
+        break; //mot-clé qui permet de sortir de ce cas pour
+
+ // si pas de break : on passe au bloc suivant /!\
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(3);
+        break;
+    case 3:
+        console.log(4);
+        break;
+    case 4:
+        console.log(5);
+        break;
+    default:
+        console.log("(╯°□°)╯︵ ┻━┻");
+}
